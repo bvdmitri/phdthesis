ef34b68ff4b2976b08dee47be7845f106f000915
Bagaev Dmitry <bvdmitri@gmail.com>
2023-05-30 11:38:50 +0200

    update

---
+++ src/models/forneylab/doublependulum.jl
@@ -0,0 +1,116 @@
+# NOTE: this file is not included in the project by default,
+# it must be included explicitly in the notebook experiments
+
+import ForneyLab: unsafeMean, unsafeCov
+
+const flmodels = Dict()
+
+function make_forneylab_model(T; id = "")
+    # This creates new global variable for the current factor graph
+    g = FactorGraph()
+    
+    @RV σ ~ Gamma(0.001, 0.01)
+
+    # Pre-define vectors for storing latent and observed variables
+    # To access the result we therefore need to define more latent variables
+    h = Vector{Variable}(undef, T - 1) 
+    u = Vector{Variable}(undef, T)
+    s = Vector{Variable}(undef, T) 
+    y = Vector{Variable}(undef, T)
+
+    @RV s[1] ~ GaussianMeanPrecision(zeros(4), Matrix(I(4)))
+    @RV u[1] = dot(s[1], [ 0.0, 1.0, 0.0, 0.0 ])
+    @RV y[1] ~ GaussianMeanPrecision(u[1], σ)
+    
+    # Indicate observed variable
+    placeholder(y[1], :y, index = 1)
+    
+    for t in 2:T
+        @RV h[t - 1] ~ Delta{Extended}(s[t - 1]; g = f)
+        @RV s[t] ~ GaussianMeanPrecision(h[t - 1], Matrix(1e5 * I(4)))
+        @RV u[t] = dot(s[t], [ 0.0, 1.0, 0.0, 0.0 ])
+        @RV y[t] ~ GaussianMeanPrecision(u[t], σ)
+
+        # Indicate observed variable
+        placeholder(y[t], :y, index = t) 
+    end
+    
+    # q(σ, u) = q(σ)q(u)
+    q = PosteriorFactorization(σ, u, ids=[:σ, :u])
+    
+    # Build the variational update algorithms for each posterior factor
+    algo = messagePassingAlgorithm(id = Symbol(string("a", id)))
+
+    # Generate source code for the algorithms
+    source_code = algorithmSourceCode(algo)
+    
+    # ForneyLab returns generated code as a string
+    parsed = Meta.parse(source_code)
+    
+    # ForneyLab evaluates the generated code dynamically
+    eval(parsed)
+    
+    # ForneyLab creates global functions dynamically
+    # That is a bit unfortunate as it does not play nicely if we want 
+    # to perform benchmarks in a loop
+    # Here we attempt to get the references for the generated functions dynamically
+    # and return them separately
+    steps = () -> begin
+        stepσ = Base.getproperty(Main, Symbol(string("stepa", id, "σ!")))
+        stepu = Base.getproperty(Main, Symbol(string("stepa", id, "u!")))
+        initu = Base.getproperty(Main, Symbol(string("inita", id, "u")))
+        return (
+            (args...) -> Base.invokelatest(stepσ, args...), 
+            (args...) -> Base.invokelatest(stepu, args...), 
+            (args...) -> Base.invokelatest(initu, args...)
+        )
+    end
+    
+    return steps
+end
+
+# ForneyLab needs to compile the model first
+function get_forneylab_model(T, seed; force = false)
+    id = string(T, seed) # unique id
+    # do not recompile the model if not needed
+    if !force
+        return get!(() -> make_forneylab_model(T, id = id), flmodels, id)
+    else
+        model = make_forneylab_model(T, id = id)
+        flmodels[id] = model
+        return model
+    end
+end
+
+# First invoke slow as usual, but we do not perform benchmarks here so it does not really matter
+# We perform a small benchmark below, for that we need to wrap the `inference` call in a separate function
+function run_inference(flmodel, observations)
+    (stepσ!, stepu!, initu) = flmodel()
+    
+    data = Dict(
+        :y => observations,
+    )
+    
+    marginals = Dict{Any, Any}(
+        :σ => ProbabilityDistribution(Gamma, a = 0.001, b = 0.01)
+    )
+    
+    messages = initu()
+    
+    for vmp in 1:4
+        stepu!(data, marginals, messages)
+        stepσ!(data, marginals, messages)
+    end
+    
+    return marginals
+end
+
+function extract_forneylab_marginals(T, results)
+    sposteriors = []
+    push!(sposteriors, (unsafeMean(results[:s_1]), sqrt(unsafeCov(results[:s_1]))))
+    for t in 2:T
+        sym = Symbol(string("s_", t, "_h_", t - 1))
+        push!(sposteriors, (unsafeMean(results[sym])[1:4], sqrt(unsafeCov(results[sym])[1:4, 1:4])))
+    end
+    return sposteriors
+end
--- src/models/rxinfer/doublependulum.jl
+++ src/models/rxinfer/doublependulum.jl
@@ -12,16 +12,16 @@ ReactiveMP.fastcholesky(x::SMatrix) = ReactiveMP.fastcholesky(Matrix(x))
 # - state + N(0, precision) is replaced with N(state, precision) as they are identical, but generate less nodes
 # - arrays are replaced with their static array equivalents with the SA prefix
 # - variable names are different, but that is minor IMO
-@model function double_pendulum(T, s_start)
+@model function double_pendulum(T)
     
     s = randomvar(T)
     y = datavar(Float64, T)
-    σ ~ Gamma(0.001, 100.0)
+    σ ~ Gamma(shape = 0.001, rate = 0.01)
     
-    Σ = constvar(SMatrix{4, 4}(1000 * diageye(4)))
+    Σ = constvar(SMatrix{4, 4}(1e5 * diageye(4)))
     c = constvar(SA[ 0.0, 1.0, 0.0, 0.0 ])
     
-    s[1] ~ MvNormal(mean = s_start, covariance = SMatrix{4, 4}(diageye(4)))
+    s[1] ~ MvNormal(mean = zeros(4), covariance = SMatrix{4, 4}(diageye(4)))
     y[1] ~ Normal(mean = dot(s[1], c), precision = σ)
     
     for t in 2:T
@@ -36,4 +36,19 @@ end
 
 @constraints function double_pendulum_constraints()
     q(s, σ) = q(s)q(σ)
+end
+
+# First invoke slow as usual, but we do not perform benchmarks here so it does not really matter
+# We perform a small benchmark below, for that we need to wrap the `inference` call in a separate function
+function run_inference(rximodel, observations; iterations = 5)
+    return inference(
+        model = rximodel,
+        data = (y = observations, ),
+        meta = double_pendulum_meta(),
+        constraints = double_pendulum_constraints(),
+        returnvars = (s = KeepLast(), ),
+        iterations = iterations,
+        initmarginals = (σ = GammaShapeRate(0.001, 0.01), ),
+        options = (limit_stack_depth = 500, )
+    )
 end
